f9e9335a1674293731c34fd10a6f199b897f26f1
nightotos <nightotos@gmail.com>
2019-01-11 00:48:46 -0500

    guardado de informacion en base de datos relacional
    Signed-off-by: nightotos <nightotos@gmail.com>

--- src/empresa/proyecto/app/aplicacion.java
+++ src/empresa/proyecto/app/aplicacion.java
@@ -1,5 +1,12 @@
 package empresa.proyecto.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class aplicacion {
 
     public aplicacion() {
@@ -8,16 +15,47 @@ public class aplicacion {
     
     
    public static void main (String args[]){
-      int a= 20;
-      int b= 4;
+      
+      //CONEXION A LA BASE DE DATOS
+      Connection miConexion;
+      //INICIO
+      String url="jdbc:mysql://localhost:3306/soap";
+      String user="soap";
+      String password="soap";
+      
+      //DECLARACION DE VARIABLES
+      int a= 30;
+      int b= 5;
       //llamado a la funcion add
-      float Add= add(a,b);
+      int Add= add(a,b);
       //llamado a la funcion divide
-      float Divide= divide(a,b);
+      int Divide= divide(a,b);
       //llamado a la funcion multiplly
-      float Multiply= multiply(a,b);
+      int Multiply= multiply(a,b);
       //llamado a la funcion subtract
-      float subtract= subtract(a,b);
+      int subtract= subtract(a,b);
+      
+      try {
+            miConexion=DriverManager.getConnection(url, user, password);
+            //CONEXION ALA BASE DE DATOS
+            
+            String sql_add="INSERT INTO `soap_add`( `a`, `b`, `resultado`) VALUES ( "+a+","+b+","+Add+");";
+            String sql_divide="INSERT INTO `soap_divide`( `a`, `b`, `resultado`) VALUES ( "+a+","+b+","+Divide+");";
+            String sql_multiply="INSERT INTO `soap_multiply`( `a`, `b`, `resultado`) VALUES ( "+a+","+b+","+Multiply+");";
+            String sql_subtract="INSERT INTO `soap_subtract`( `a`, `b`, `resultado`) VALUES ( "+a+","+b+","+subtract+");";
+            Statement clausula;
+            
+            clausula=miConexion.createStatement();
+            
+            clausula.execute(sql_add);
+            clausula.execute(sql_divide);
+            clausula.execute(sql_multiply);
+            clausula.execute(sql_subtract);
+            //PREPARAR LA CONSULTA
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(aplicacion.class.getName()).log(Level.SEVERE, null, ex);
+        }
       
       
        
